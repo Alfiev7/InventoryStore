638fdf13b23926c5e099406e5db7402963f05da9
Alfiev7 <hoopsnfries@gmail.com>
2025-02-07 15:20:08 +0200

    Fix-DRY-AuthenticationForm.jsx

---
+++ frontend/src/components/Login/AuthenticationForm.jsx
@@ -0,0 +1,18 @@
+export default function AuthenticationForm({
+  title,
+  subtitle,
+  onSubmit,
+  children,
+}) {
+  return (
+    <form onSubmit={onSubmit} className="flex flex-col gap-5 items-center">
+      <div className="flex flex-col gap-1 items-center">
+        <h1 className="text-2xl">{title}</h1>
+        {subtitle && (
+          <h2 className="italic text-gray-500 text-sm">{subtitle}</h2>
+        )}
+      </div>
+      {children}
+    </form>
+  );
+}
