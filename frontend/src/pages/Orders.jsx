d9ff41a46707b1cbf10674c9789572c9096a3ece
Alfiev7 <141186039+Alfiev7@users.noreply.github.com>
2025-02-15 17:54:44 +0200

    orders page, mock data, OrdersTable, Orders.jsx

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,7 +1,18 @@
+import OrdersTable from '../components/Orders/OrdersTable';
+import { useNavigate } from 'react-router-dom';
+
 export default function Orders() {
+  const navigate = useNavigate();
+
+  const onOrderClick = (orderId) => {
+    console.log('Navigating to order:', orderId);
+    navigate(`/orders/${orderId}`);
+  };
+
   return (
-    <div>
-      <h1>Orders</h1>
+    <div className="p-8 bg-gray-100 min-h-screen flex flex-col items-center">
+      <h2 className="text-3xl font-semibold text-purple-700 mb-6">Orders</h2>
+      <OrdersTable onOrderClick={onOrderClick} />
     </div>
   );
 }
