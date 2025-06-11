5871fd344671d8121cdcd6f3df61e329c83ca559
Alfiev7 <141186039+Alfiev7@users.noreply.github.com>
2025-06-11 13:20:53 +0300

    Create /v1/users/login (POST) route

--- backend/server/src/users/users.service.ts
+++ backend/server/src/users/users.service.ts
@@ -3,8 +3,19 @@ import mockUsers from './mock-users.json';
 
 let users = [...mockUsers];
 
+type LoginStatus = 'success' | 'failed' | 'unauthorized';
+
 @Injectable()
 export class UsersService {
+  loginUser(username: string, password: string): { status: LoginStatus } {
+    const user = users.find(
+      (u) => u.username === username && u.password === password,
+    );
+
+    const status: LoginStatus = user ? 'success' : 'unauthorized';
+    return { status };
+  }
+
   getUserByEmail(email: string) {
     return users.find((user) => user.email === email);
   }
@@ -14,7 +25,7 @@ export class UsersService {
   }
 
   createUser(userData: any) {
-    let newUser = {id: users.length + 1, ...userData};
+    let newUser = { ...userData, id: users.length + 1 };
     users.push(newUser);
     return newUser;
   }
